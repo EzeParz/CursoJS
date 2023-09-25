c3cf1f076441d4a85f747b5c8603dbe84e3fa3d9
Eze Parz <ezeyamp15@outlook.es>
2023-09-25 19:38:39 -0300

    Modifique la 3era. Preentrega

--- js/formularioDeContacto3eraPreentrega.js
+++ js/formularioDeContacto3eraPreentrega.js
@@ -1,40 +1,48 @@
 //@ts-nocheck
 
-const formularioDeContacto = document.querySelector("#formularioDeContacto")
-const inputNombre = document.querySelector("#nombre")
-const inputEmail = document.querySelector("#email")
-const inputTelefono = document.querySelector("#telefono")
-const inputMensaje = document.querySelector("#mensaje")
+const formularioDeContacto = document.querySelector("#formularioDeContacto");
+const inputNombre = document.querySelector("#nombre");
+const inputEmail = document.querySelector("#email");
+const inputTelefono = document.querySelector("#telefono");
+const inputMensaje = document.querySelector("#mensaje");
+const lista = document.querySelector("#listaDeFormularios");
 
-
-const formularios = JSON.parse(localStorage.getItem("formularios")) || []
 
 class Formularios{
     constructor({nombre,email,telefono,mensaje}){
     this.nombre = nombre;
-    this.email = email
-    this.telefono = telefono
-    this.mensaje = mensaje
+    this.email = email;
+    this.telefono = telefono;
+    this.mensaje = mensaje;
     }
 }
 
 formularioDeContacto.onsubmit = e => {
     e.preventDefault()
    
-    const nombre = inputNombre.value 
-    const email = inputEmail.value 
-    const telefono = inputTelefono.value 
-    const mensaje = inputMensaje.value
+    const nombre = inputNombre.value; 
+    const email = inputEmail.value; 
+    const telefono = inputTelefono.value; 
+    const mensaje = inputMensaje.value;
     
-    const nuevoFormulario = new Formularios({nombre, email, telefono, mensaje})
+    const nuevoFormulario = new Formularios({nombre, email, telefono, mensaje});
 
-    guardarNuevoFormulario(nuevoFormulario)
-    console.log(nuevoFormulario)
+    guardarNuevoFormulario(nuevoFormulario);
+    mostrarNuevoFormulario(nuevoFormulario);
 }
 
-function guardarNuevoFormulario(nuevoFormulario){
-    formularios.push(nuevoFormulario)
-    localStorage.setItem("formularios", JSON.stringify(formularios))  
+function guardarNuevoFormulario(nuevoFormulario) {
+    const formulariosGuardados = JSON.parse(localStorage.getItem("formularios")) || [];
+    formulariosGuardados.push(nuevoFormulario);
+    localStorage.setItem("formularios", JSON.stringify(formulariosGuardados));
 }
+
+
+function mostrarNuevoFormulario(nuevoFormulario){
+    const listItem = document.createElement("p");
+    listItem.textContent = `Nombre: ${nuevoFormulario.nombre}, Email: ${nuevoFormulario.email}, Teléfono: ${nuevoFormulario.telefono}, Mensaje: ${nuevoFormulario.mensaje}`;
+    lista.appendChild(listItem);
+}
+
 
 
